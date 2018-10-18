142115c711cbac65d9134fd2923306e7bfb36acd
kcolyhs <bchen919@gmail.com>
2018-10-18 17:44:25 -0400

    Initial commit

---
+++ KnowledgeBaseMinesweeper.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from enum import Enum
+
+class Predicate(Enum):
+    true = 1
+    false = 2
+    undetermined = 3
+
+
+class KnowledgeBase():
+    def __init__(self,length,width):
+        self.minedPre
+        
+    
---
+++ Minesweeper.py
@@ -0,0 +1,29 @@
+from KnowledgeBaseMinesweeper import KnowledgeBase,Predicate
+
+class GameTile:
+    def __init__(self):
+        pass
+
+class MineSweeper:
+    def __init__(self):
+        self.playing = False;
+        self.kb = None
+        
+    def new_game(self):
+        #Query the user for a width and length
+        length = int(input("What is the length of the game board?"))
+        width = int(input("What is the width of the game board?"))
+        self.kb = KnowledgeBase(length,width)
+        self.playing = True;
+        
+    def play_game(self):
+        if not self.playing:
+            #tried to play gmae whne not possible
+            pass
+        #decide on which 
+    
+if __name__ == '__main__':
+    minesweeper = MineSweeper()
+    minesweeper.new_game()
+    
+        
