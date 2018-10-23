f92d1d8464bc45fce9d1c9743aa55c75bdf23e97
kcolyhs <bchen919@gmail.com>
2018-10-23 15:36:09 -0400

    Reorganizing the data structures in KB
    ...in order to more easily keep track of satisfied/unsatisfied
    constraints

--- KnowledgeBaseMinesweeper.py
+++ KnowledgeBaseMinesweeper.py
@@ -1,6 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from enum import Enum
+
 
 class Predicate(Enum):
     true = 1
@@ -10,14 +9,38 @@ class Predicate(Enum):
 
 class KnowledgeBase():
     def __init__(self, length, width):
-        self.mine_predicates_arr = [[Predicate.undetermined for y in range(length)] for x in range(width)]
-        self.tile_arr= [[Tile(False) for y in range(length)] for x in range(width)]
+        self.length = length
+        self.width = width
+        self.tile_arr = [[Tile(x,y) for y in range(length)]
+                         for x in range(width)]
+        self.satisfied
+
+    def in_bounds(self, x, y):
+        return not ((x < 0 or x >= self.width)
+                    or (y < 0 or y >= self.length))
+
+    def check_tile_for_conflicts(self, tile):
+        count = 0
+        x = tile.x
+        y = tile.y
+        # Count adjacent mines
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if (self.in_bounds(i, j) and
+                        self.tile_arr[i][j] == Predicate.true
+                        and not (i == x and j == y)):
+                    count += 1
+        if count >= tile.adj_mines:
+            return True
+
 
 class Tile():
-    def __init__(self, discovered):
-        self.discovered = discovered
+    def __init__(self, x, y):
+        self.visited = False
+        self.is_mined = Predicate.undetermined
         self.adj_mines = -1
-        self.satisfied = True
+        self.x = x
+        self.y = y
 
 
 if __name__ == '__main__':
--- MinesweeperAgent.py
+++ MinesweeperAgent.py
@@ -1,90 +1,108 @@
-from KnowledgeBaseMinesweeper import KnowledgeBase,Predicate
+from KnowledgeBaseMinesweeper import KnowledgeBase, Predicate
 from MinesweeperGame import MinesweeperGame
-
+from copy import deepcopy
 
 class GameTile:
     def __init__(self):
         pass
 
+
 class MineSweeperAgent:
     def __init__(self):
         self.gameType = None
         self.kb = None
-        
+
     def new_cpu_game(self):
         # Query the user for a width and length
         length = int(input("What is the length of the game board?"))
         width = int(input("What is the width of the game board?"))
         num_mines = int(input("How many mines do you want the generated\
                               map to have?"))
-        self.kb = KnowledgeBase(length,width)
-        self.game = MinesweeperGame(length,width,num_mines)
+
+        self.length = length
+        self.width = width
+        self.kb = KnowledgeBase(length, width)
+        self.game = MinesweeperGame(length, width, num_mines)
         self.game_type = "CPU"
-        
+
     def new_human_game(self):
         # Query the user for a width and length
         length = int(input("What is the length of the game board?"))
         width = int(input("What is the width of the game board?"))
-        self.kb = KnowledgeBase(length,width)
+        self.kb = KnowledgeBase(length, width)
         self.game_type = "HUMAN"
-        
+#
     def play_game(self):
         if self.gameType is not None:
-            #tried to play game when not possible
+            # tried to play game when not possible
             pass
         # decide on which tile to query
         # perform PBC if nothing to safely visit
         # guess if there is nothing clear and PBC on every viable option
 
+    def process_query(self, num, cur_tile):
+        if num is 9:
+            # TODO gameover
+            return
+        elif num >= 0 and num <= 8:
+            cur_tile.discovered = True
+            cur_tile.adj_mines = num
+            cur_tile.is_mined = Predicate.false
+        else:
+            # Error invalid num
+            return
+
     def query_tile_human(self, x, y):
         # returns 0-8 for num of adj mines or 9 if the tile is mined
-        pass
+        querystring = "What is in space ({},{})", (x, y)
+        num = int(input(querystring))
+        cur_tile = self.kb.tile_arr[x][y]
+        self.process_query(num, cur_tile)
 
     def query_tile_cpu(self, x, y):
         # returns 0-8 for num of adj mines or 9 if the tile is mined
-        return self.game.grid[x][y]
-        
+        num = self.game.grid[x][y]
+        cur_tile = self.kb.tile_arr[x][y]
+        self.process_query(num, cur_tile)
 
     def proof_by_contradiction(self, x, y):
+        cur_tile = self.kb.tile_arr[x][y]
         # Returns a predicate on if the arg tile is mined
-        predicates = self.kb.mine_predicates_arr
-        if predicates[x][y] is not Predicate.undetermined:
+        if cur_tile.is_mined is not Predicate.undetermined:
             # Error no need to do PBC on an already known tile
             return
-            
-        def try_to_satisfy(x, y):
-            kb = self.kb
-            result = False
-            # TODO
-            '''
-            1. Check if the current setup conflicts
-            2. Add mines to try and satisfy
-            3. If not possible
-            '''
-            return result
-            pass
-
         # Add p to KB and try to satisfy
-        predicates[x][y] = Predicate.true
-        p1 = try_to_satisfy(x,y)
+        cur_tile.is_mined = Predicate.true
+        p1 = self.try_to_satisfy(x, y)
         # Add not p to KB and try to satisfy
-        predicates[x][y] = Predicate.false
-        p2 = try_to_satisfy(x,y)
+        cur_tile.is_mined = Predicate.false
+        p2 = self.try_to_satisfy(x, y)
 
         if p1 and p2:
-            predicates[x][y] = Predicate.undetermined
+            pass
         elif p1 and not p2:
-            predicates[x][y] = Predicate.true
+            cur_tile.is_mined = Predicate.true
         elif not p1 and p2:
-            predicates[x][y] = Predicate.false
+            cur_tile.is_mined = Predicate.false
         else:
-            # Error 
+            # Error
             return
         return
+
+    def try_to_satisfy(self, x, y):
+        test_kb = deepcopy(self.kb)
+
+        result = False
+        # TODO
+        '''
+        1. Check if the current setup conflicts
+        2. Add mines to try and satisfy
+        3. If not possible
+        '''
+        del(test_kb)
+        return result
 
 
 if __name__ == '__main__':
     agent = MineSweeperAgent()
     agent.new_cpu_game()
-    
-        
