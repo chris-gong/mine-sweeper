fa972e37a88acfac450f55b2c7bfbb03a5e0369b
kcolyhs <bchen919@gmail.com>
2018-10-25 00:01:02 -0400

    changed render code to 0,0 shows up on top left (axes don't align

--- MinesweeperGame.py
+++ MinesweeperGame.py
@@ -50,6 +50,8 @@ class MinesweeperGame:
 
     def render_grid(self):
         # cmap = colors.ListedColormap(['white', 'red', 'red', 'red', 'blue'])
+        arraycopy = np.copy(self.grid)
+        arraycopy = np.rot90(arraycopy,3)
         colors0 = plt.cm.binary(np.linspace(0, 1, 1))
         colors1 = plt.cm.Reds(np.linspace(.2, .8, 8))
         # colors1 = np.flip(colors1,0)
@@ -64,7 +66,7 @@ class MinesweeperGame:
         fig, ax = plt.subplots()
         ax.imshow(self.grid, cmap=cmap, norm=norm)
         '''
-        plt.pcolor(self.grid, cmap=mymap)
+        plt.pcolor(arraycopy, cmap=mymap)
         plt.colorbar()
         plt.show()
         '''
